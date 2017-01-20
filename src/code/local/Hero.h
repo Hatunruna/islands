512adba0bbec0eabff2b7c3fd4d4fd45a2925c5a
ahugeat <hugeat.arthur@gmail.com>
2017-01-20 20:09:15 +0100

    Add Hero class.

---
+++ src/code/local/Hero.h
@@ -0,0 +1,17 @@
+// TODO add licence
+
+#ifndef LOCAL_HERO_H
+#define LOCAL_HERO_H
+
+#include <gf/Entity.h>
+
+namespace bi {
+  class Hero: public gf::Entity {
+  public:
+    //
+
+  private:
+  }
+}
+
+#endif // LOCAL_HERO_H
